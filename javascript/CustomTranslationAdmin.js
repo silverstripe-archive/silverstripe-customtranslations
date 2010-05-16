35b6403d63415a7e4cf2de103bb1326344fd5897
Mark Stephens <mark@silverstripe.com>
2010-05-16 23:33:03 +0000

    BUGFIX: reload cms after delete. Not ideal, but there is no easy way to reload just the table

--- javascript/CustomTranslationAdmin.js
+++ javascript/CustomTranslationAdmin.js
@@ -1,32 +1,29 @@
 // Override the default delete behaviour. We don't want to delete the row client side, but rather refresh
-// the RHS
+// the RHS.
 TableListField.prototype.deleteRecord = function(e) {
-		var img = Event.element(e);
-		var link = Event.findElement(e,"a");
-		var row = Event.findElement(e,"tr");
+	var img = Event.element(e);
+	var link = Event.findElement(e,"a");
+	var row = Event.findElement(e,"tr");
 
-		// TODO ajaxErrorHandler and loading-image are dependent on cms, but formfield is in sapphire
-		var confirmed = confirm(ss.i18n._t('TABLEFIELD.DELETECONFIRMMESSAGE', 'Are you sure you want to delete this record?'));
-		if(confirmed)
-		{
-			img.setAttribute("src",'cms/images/network-save.gif'); // TODO doesn't work
-			new Ajax.Request(
-				link.getAttribute("href"),
-				{
-					method: 'post',
-					postBody: 'forceajax=1' + ($('SecurityID') ? '&SecurityID=' + $('SecurityID').value : ''),
-					onComplete: function(){
-						$('Form_ResultsForm_CustomLanguageTranslation').refresh();
-//						$('Form_ResultsForm_CustomLanguageTranslation').refresh();
-					}.bind(this),
-					onFailure: this.ajaxErrorHandler
-				}
-			);
-		}
-		Event.stop(e);
+	var confirmed = confirm(ss.i18n._t('TABLEFIELD.DELETECONFIRMMESSAGE', 'Are you sure you want to delete this record?'));
+	if(confirmed)
+	{
+		var oldImgSrc = img.getAttribute("src");
+		img.setAttribute("src",'cms/images/network-save.gif'); // TODO doesn't work
+		new Ajax.Request(
+			link.getAttribute("href"),
+			{
+				method: 'post',
+				postBody: 'forceajax=1' + ($('SecurityID') ? '&SecurityID=' + $('SecurityID').value : ''),
+				onComplete: function(){
+					// Each <td> in the row will contain <a ...>text</a>. Replace these td's with just
+					// the text.
+					window.location.reload();
+//					$('Form_ResultsForm_CustomLanguageTranslation').refresh();
+				}.bind(this),
+				onFailure: this.ajaxErrorHandler
+			}
+		);
 	}
-
-/*
-// This goes to the next page
-	http://localhost/wf/admin/translations/CustomLanguageTranslation/ResultsForm/field/CustomLanguageTranslation?Locale=en&Entity=&Translation=%25&ResultAssembly%5BLocale%5D=Locale&ResultAssembly%5BTranslation%5D=Translation&ResultAssembly%5BEntity%5D=Entity&ctf[CustomLanguageTranslation][start]=30
-*/
+	Event.stop(e);
+}
